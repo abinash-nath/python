f27ab38c60c27267f0403373a5528a118296b7fb
abinash-nath <abinashnath007@gmail.com>
2024-05-10 22:51:46 -0400

    list

---
+++ L10_list.py
@@ -0,0 +1,14 @@
+food = ["rice","daal","sabzi","paneer","chicken"]
+
+food[0] = "biryani"
+print(food)
+
+#food.append("ice cream")
+#food.remove("biryani")
+#food.pop()
+#food.insert(0,"cake")
+#food.sort(reverse=True)
+food.clear()
+
+for x in food:
+    print(x)
