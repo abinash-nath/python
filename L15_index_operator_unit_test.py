29d8a67f94189588bda26cf8b5a84d4a17df7e2d
abinash-nath <abinashnath007@gmail.com>
2025-07-23 19:06:18 -0400

    l15_index_operator unit test created

---
+++ L15_index_operator_unit_test.py
@@ -0,0 +1,21 @@
+import unittest
+from L15_index_operator import fruits
+
+# test_L15_index_operator.py
+
+
+class TestFruitsList(unittest.TestCase):
+    def test_index_of_apple(self):
+        self.assertEqual(fruits.index("apple"), 0)
+
+    def test_first_element(self):
+        self.assertEqual(fruits[0], "apple")
+
+    def test_third_element(self):
+        self.assertEqual(fruits[2], "cherry")
+
+    def test_apple_count(self):
+        self.assertEqual(fruits.count("apple"), 2)
+
+if __name__ == "__main__":
+    unittest.main()
